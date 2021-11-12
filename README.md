d7a76a6b786cddacb3fbd9ce747aece5edc83bc6
momoladebrouill <momoladebrouill@gmail.com>
2021-11-12 21:01:16 +0100

    je refais de zéro, et ça fonctionne plutôt bien

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# vector map
+# Flow map
+Each point is randomly chosen on the map, the closer the particule of the fluid is, the more it tend to be attracted to it
+[largely inspired by this video](https://www.youtube.com/watch?v=JlU3GskkcUw)
  
--- main.py
+++ main.py
@@ -1,48 +1,78 @@
+import math
+from random import random,choice
 import pygame as pg
-from utis import *
-from dee import Table
-from random import random
-pg.init()
-f=pg.display.set_mode((500,500),pg.RESIZABLE)
-fps=pg.time.Clock()
-B=True
+#from perlin_noise import PerlinNoise
 
+#noise = PerlinNoise(octaves=10, seed=1)
+screen=(500,500)
 
-class Particule:
-    def __init__(self,pos):
-        self.anchor=Pos(pos[0],pos[1])
-        self.pos=Pos(pos[0]+random()-.5,pos[1]+random()-.5)
-        self.vec=Vec(x=0,y=0)
-    def __repr__(self):
-        return "P"
-    
-Map=Table(size=(10,10),default=lambda x,y:Particule((x,y)))
-
-
-while B:
-    pg.display.flip()
-    f.fill(0)
-    fps.tick(60)
-    
-    for pouss,pos in Map:
-        """grav=Vec(a=pouss.pos,b=pouss.anchor)
-        grav.long/=2
-        pouss.vec+=grav
-        pouss.pos+=pouss.vec"""
-        pg.draw.circle(f,0xffffff,(pouss.pos.x*50,pouss.pos.y*50),5)
+class Ball:
+    def __init__(self,x,y):
+        self.x,self.y=x,y
+        self.vecx,self.vecy=0,0
         
-    mousepos=pg.mouse.get_pos()
+        self.aimx,self.aimy=choice(aims)
+        self.coul=0xaaaaaa
+        self.dist=0
+        
+    @property
+    def pos(self):
+        return self.x,self.y
     
-    try:
-        victim=Map[mousepos[0]//50,mousepos[1]//50]
-        repou=Vec(a=Pos(mousepos[0],mousepos[1]),b=victim.pos)
-        repou.long=1
-        victim.vec+=repou
-    except IndexError:
-        pass
-    
+    def move(self):
+        self.aimx,self.aimy=mycurrentaim(self.x,self.y)
+        self.vecx,self.vecy=((self.aimx-self.x)/100),((self.aimy-self.y)/100)
+        
+        
+        self.x+=self.vecx
+        self.y+=self.vecy
+        self.dist+=1
+        if not 0<self.x<screen[0] or not 0<self.y<screen[1] or round(self.vecx,1)==round(self.vecy,1)==0 or self.dist>50:
+            balls.remove(self)
+            new_ball()
+
+    def draw(self):
+        pg.draw.rect(f,self.coul,(self.x,self.y,1,1))
+def closest(x,y):
+    ls=[(math.sqrt((x-pos[0])**2+(y-pos[1])**2),pos) for pos in aims]
+    ls.sort(key=lambda i:i[0])
+    return ls[0][1]
+def mycurrentaim(x,y):
+    ls=[(math.sqrt((x-pos[0])**2+(y-pos[1])**2),pos) for pos in aims]
+    ls.sort(key=lambda i:i[0])
+    totaldist=sum([key[0] for key in ls])
+    aim=[0,0]
+    for dist,pos in ls:
+        aim[0]+=pos[0]/dist
+        aim[1]+=pos[1]/dist
+    aim[0]*=10
+    aim[1]*=10
+    return aim
+def new_ball():
+    balls.append(Ball(*[int(random()*dim) for dim in screen]))
+
+pg.init()
+f=pg.display.set_mode(screen)
+mask=pg.Surface(screen,0)
+mask.set_alpha(1)
+
+aims=[[int(random()*dim) for dim in screen] for _ in range(10)]
+balls=[Ball(*[int(random()*dim) for dim in screen]) for _ in range(500)]
+b=True
+fps=pg.time.Clock()
+while b:
+    fps.tick(60)
+    pg.display.update()
+    f.blit(mask,(0,0))
+    for ball in balls:
+        ball.draw()
+        ball.move()
+    for pos in aims:
+        pg.draw.circle(f,0xff60aa,pos,5)
+        
     for event in pg.event.get():
         if event.type==pg.QUIT:
-            pg.quit()
-            B=False
+            b=pg.quit()
+        elif event.type==pg.MOUSEBUTTONUP:
+            new_ball()
             
