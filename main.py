2ec05ef4830c8562571b79f14751aa48bfd9ee0b
momoladebrouill <momoladebrouill@gmail.com>
2021-11-14 18:32:31 +0100

    hihi maintenat c'est joli

--- main.py
+++ main.py
@@ -1,17 +1,18 @@
 import math
-from random import random,choice
+from random import random,choice,seed
 import pygame as pg
-from perlin_noise import PerlinNoise
-
-noise = PerlinNoise(octaves=10, seed=1)
+import noise
+from colorsys import hsv_to_rgb
 screen=(500,500)
-
+sizeXspeed=5
+depx=0
+alpha=1
 class Ball:
     def __init__(self,x,y):
         self.x,self.y=x,y
         self.vecx,self.vecy=0,0
-        self.aimx,self.aimy=choice(aims)
-        self.coul=0xaaaaaa
+ 
+        
         self.dist=0
         
     @property
@@ -19,9 +20,11 @@ class Ball:
         return self.x,self.y
     
     def move(self):
-        self.aimx,self.aimy=mycurrentaim(self.x,self.y)
-        self.vecx,self.vecy=((self.aimx-self.x)/100),((self.aimy-self.y)/100)
-        
+        val=noise.pnoise2(self.x/screen[0],self.y/screen[1],octaves=10,base=depx)
+        self.coul=hsv_to_rgb((val+1),1,255)
+        self.vecx=math.cos(val*math.pi)*sizeXspeed
+        self.vecy=math.sin(val*math.pi)*sizeXspeed
+                
         
         self.x+=self.vecx
         self.y+=self.vecy
@@ -31,31 +34,17 @@ class Ball:
             new_ball()
 
     def draw(self):
-        pg.draw.rect(f,self.coul,(self.x,self.y,1,1))
-def closest(x,y):
-    ls=[(math.sqrt((x-pos[0])**2+(y-pos[1])**2),pos) for pos in aims]
-    ls.sort(key=lambda i:i[0])
-    return ls[0][1]
-def mycurrentaim(x,y):
-    ls=[(math.sqrt((x-pos[0])**2+(y-pos[1])**2),pos) for pos in aims]
-    ls.sort(key=lambda i:i[0])
-    totaldist=sum([key[0] for key in ls])
-    aim=[0,0]
-    for dist,pos in ls:
-        aim[0]+=pos[0]/dist
-        aim[1]+=pos[1]/dist
-    aim[0]*=10
-    aim[1]*=10
-    return aim
+        pg.draw.circle(f,self.coul,self.pos,sizeXspeed)
+
+
 def new_ball():
     balls.append(Ball(*[int(random()*dim) for dim in screen]))
 
 pg.init()
-f=pg.display.set_mode(screen)
+f=pg.display.set_mode(screen,pg.RESIZABLE)
 mask=pg.Surface(screen,0)
-mask.set_alpha(1)
+mask.set_alpha(alpha)
 
-aims=[[int(random()*dim) for dim in screen] for _ in range(10)]
 balls=[Ball(*[int(random()*dim) for dim in screen]) for _ in range(500)]
 b=True
 fps=pg.time.Clock()
@@ -64,14 +53,29 @@ while b:
     pg.display.update()
     f.blit(mask,(0,0))
     for ball in balls:
-        ball.draw()
+        
         ball.move()
-    for pos in aims:
-        pg.draw.circle(f,0xff60aa,pos,5)
+        ball.draw()
+
         
     for event in pg.event.get():
         if event.type==pg.QUIT:
             b=pg.quit()
         elif event.type==pg.MOUSEBUTTONUP:
+            if event.button==4 or event.button==5:
+                sizeXspeed+=1 if event.button==4 else -1
             new_ball()
+            seed()
+        elif event.type==pg.KEYUP:
+            if event.key==pg.K_UP:
+                depx+=1
+            elif event.key==pg.K_DOWN:
+                depx-=1
+        elif event.type==pg.VIDEORESIZE:
+            screen=event.w,event.h
+            mask=pg.Surface(screen,0)
+            mask.set_alpha(alpha)
+            balls=[Ball(*[int(random()*dim) for dim in screen]) for _ in range(1000)]
+              
+                
             
